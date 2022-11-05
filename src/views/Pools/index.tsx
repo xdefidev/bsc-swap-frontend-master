16401aa014e66ad0cd775e9b862ee0281a1c71e1
xdefidev <xdefidev@gmail.com>
2022-11-05 15:29:03 +0100

    changes

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -240,7 +240,7 @@ const Pools: React.FC = () => {
         <Flex justifyContent="space-between" flexDirection={['column', null, null, 'row']}>
           <Flex flex="1" flexDirection="column" mr={['8px', 0]}>
             <Heading as="h1" scale="xxl" color="secondary" mb="24px">
-              {t('Syrup Pools')}
+              {t('Pools')}
             </Heading>
             <Heading scale="md" color="text">
               {t('Just stake some tokens to earn.')}
